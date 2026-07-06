437b4c091218009e17014b4f4b9432f4ad511526
rstiti <rabah.stiti@insa-rouen.fr>
2026-07-06 21:55:06 +0100

    feat(eda): add preprocessing of validation dataset and add clean_credit_data and cap_outliers_explicit_value in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 
 def etude_sauts_quantiles(df, colonne):
@@ -16,4 +17,37 @@ def cap_outliers_quantile(df, colonne, quantile_seuil):
 
     return df
 
+def cap_outliers_explicit_value(df, colonne, cap):
+    df[colonne] = df[colonne].clip(lower=0, upper=cap)
+    return df
 
+def clean_credit_data(df, income_imputer:SimpleImputer, age_median):
+    df = df.copy()
+
+    # Traitement des données manquantes
+    df["NumberOfDependents"] = df["NumberOfDependents"].fillna(0)
+
+    new_income = income_imputer.transform((df[["MonthlyIncome"]]))
+    df["MonthlyIncome"] = new_income[:,0]
+    df["MissingIncomeFlag"] = new_income[:,1]
+
+    # Traitement des outliers
+    df.loc[df["age"]== 0, "age"] = age_median
+    df = cap_outliers_explicit_value(df, "MonthlyIncome", 72000)
+
+    labels_past_due = [
+        "NumberOfTime30-59DaysPastDueNotWorse",
+        "NumberOfTime60-89DaysPastDueNotWorse",
+        "NumberOfTimes90DaysLate"
+    ]
+    masque_past_due_98_96 = df[labels_past_due].isin([96, 98]).any(axis=1)
+    df["Has_System_Error_98"] = masque_past_due_98_96.astype(int)
+    df.loc[masque_past_due_98_96, labels_past_due] = 0
+
+    df = cap_outliers_explicit_value(df, "RevolvingUtilizationOfUnsecuredLines", 3.28)
+    masque_missing_income_label = df["MissingIncomeFlag"] == 1
+    df.loc[masque_missing_income_label, "DebtRatio"] = df.loc[masque_missing_income_label, "DebtRatio"] / df.loc[
+        masque_missing_income_label, "MonthlyIncome"]
+    df = cap_outliers_explicit_value(df, "DebtRatio", 2.46)
+
+    return df
