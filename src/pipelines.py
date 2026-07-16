410d012a19afe56426a4fafeba1a73474e1ede3e
rstiti <rabah.stiti@insa-rouen.fr>
2026-07-16 03:32:16 +0100

    feat: add custom ColumnTransformer for missing values imputing in cleaning pipeline getter

--- src/pipelines.py
+++ src/pipelines.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 
@@ -8,5 +9,11 @@ def get_cleaner_pipeline() -> Pipeline:
     # Données manquantes
     dependents_imputer = SimpleImputer(strategy='constant', fill_value=0)
     income_imputer = SimpleImputer(strategy='median', add_indicator=True)
+    missing_values_transformer = ColumnTransformer([
+        ('dependants', dependents_imputer, ['NumberOfDependents']),
+        ('income', income_imputer, ['MonthlyIncome']),
+        ], remainder='passthrough')
 
-    return Pipeline([])
+    return Pipeline([
+        ('missing', missing_values_transformer),
+    ])
