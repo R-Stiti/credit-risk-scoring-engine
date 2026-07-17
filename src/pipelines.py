ccceb83394dc2dd2df4a30002b3117de3c7950bd
rstiti <rabah.stiti@insa-rouen.fr>
2026-07-17 03:28:26 +0100

    feat: add age imputer and NumberOfTime transformer to pretreatment pipeline and add outliers pretreatment discussion in notebook 03

--- src/pipelines.py
+++ src/pipelines.py
@@ -4,16 +4,49 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 
 
-def get_cleaner_pipeline() -> Pipeline:
+class CodesFlagger(BaseEstimator, TransformerMixin):
+    def __init__(self, columns_list : list, codes_list : list):
+        self.columns_list = columns_list
+        self.codes_list = codes_list
 
-    # Données manquantes
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None, **fit_params):
+        df = X.copy()
+
+        col_mask = df[self.columns_list].isin(self.codes_list).any(axis=1)
+        df[self.columns_list] = df[self.columns_list].replace(self.codes_list, 0)
+
+        flag_name = f"Has_System_Error_{'_'.join(map(str,self.codes_list))}"
+        df[flag_name] = col_mask.astype(int)
+        return df
+
+
+def get_cleaner_pipeline() -> Pipeline:
     dependents_imputer = SimpleImputer(strategy='constant', fill_value=0)
     income_imputer = SimpleImputer(strategy='median', add_indicator=True)
-    missing_values_transformer = ColumnTransformer([
+    age_imputer = SimpleImputer(missing_values=0, strategy='median')
+
+    imputation_transformer = ColumnTransformer([
         ('dependants', dependents_imputer, ['NumberOfDependents']),
         ('income', income_imputer, ['MonthlyIncome']),
-        ], remainder='passthrough')
+        ('age', age_imputer, ["age"])
+        ], remainder='passthrough', verbose_feature_names_out=False)
 
-    return Pipeline([
-        ('missing', missing_values_transformer),
+    code_cols = [
+    "NumberOfTime30-59DaysPastDueNotWorse",
+    "NumberOfTime60-89DaysPastDueNotWorse",
+    "NumberOfTimes90DaysLate"
+    ]
+    codes_list = [96,98]
+    codes_flagger = CodesFlagger(columns_list=code_cols, codes_list=codes_list)
+
+    # build final
+    pipeline = Pipeline([
+        ('codes flagger', codes_flagger),
+        ('imputer', imputation_transformer),
     ])
+
+    pipeline.set_output(transform='pandas')
+    return pipeline
