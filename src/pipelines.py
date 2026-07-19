1a568a4beb6c4fc446da3de925253fe1ff2a569f
rstiti <rabah.stiti@insa-rouen.fr>
2026-07-19 01:07:52 +0100

    feat: add dynamic quantile capper to pretreatment pipeline

--- src/pipelines.py
+++ src/pipelines.py
@@ -1,7 +1,10 @@
+import numpy as np
+from pandas import DataFrame
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+from kneed import KneeLocator
 
 
 class CodesFlagger(BaseEstimator, TransformerMixin):
@@ -12,7 +15,7 @@ class CodesFlagger(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X, y=None, **fit_params):
+    def transform(self, X, y=None):
         df = X.copy()
 
         col_mask = df[self.columns_list].isin(self.codes_list).any(axis=1)
@@ -20,6 +23,39 @@ class CodesFlagger(BaseEstimator, TransformerMixin):
 
         flag_name = f"Has_System_Error_{'_'.join(map(str,self.codes_list))}"
         df[flag_name] = col_mask.astype(int)
+        return df
+
+class QuantileCapper(BaseEstimator, TransformerMixin):
+    caps_ : dict
+
+    def __init__(self, columns_list : list):
+        self.columns_list = columns_list
+
+    def fit(self, X:DataFrame, y=None):
+        self.caps_ = {}
+        quantiles = np.linspace(0.90, 0.999, 100)
+        quantiles_curves = X[self.columns_list].quantile(quantiles)
+
+        for column in self.columns_list:
+            y_quantile = quantiles_curves[column]
+            knee_locator = KneeLocator(
+                x=quantiles,
+                y=y_quantile,
+                curve='convex',
+                direction='increasing'
+            )
+
+            if knee_locator.knee_y is not None:
+                self.caps_[column] = knee_locator.knee_y
+            else :
+                self.caps_[column] = y_quantile.iloc[-1]
+        return self
+
+    def transform(self, X, y=None):
+        df = X.copy()
+        for column in self.columns_list:
+            df[column] = df[column].clip(upper=self.caps_[column])
+
         return df
 
 
@@ -42,10 +78,14 @@ def get_cleaner_pipeline() -> Pipeline:
     codes_list = [96,98]
     codes_flagger = CodesFlagger(columns_list=code_cols, codes_list=codes_list)
 
+    cols_to_cap = ["MonthlyIncome", "RevolvingUtilizationOfUnsecuredLines"]
+    quantile_capper = QuantileCapper(columns_list=cols_to_cap)
+
     # build final
     pipeline = Pipeline([
         ('codes flagger', codes_flagger),
         ('imputer', imputation_transformer),
+        ('capper', quantile_capper)
     ])
 
     pipeline.set_output(transform='pandas')
