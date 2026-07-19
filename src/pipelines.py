b14589510ffe94ba07de37a8fcaa517adc2e8113
rstiti <rabah.stiti@insa-rouen.fr>
2026-07-19 02:53:06 +0100

    feat: add debt ratio fixer to pretreatment pipeline and add discussion on the matter to notebook 3

--- src/pipelines.py
+++ src/pipelines.py
@@ -58,6 +58,42 @@ class QuantileCapper(BaseEstimator, TransformerMixin):
 
         return df
 
+class DebtRatioFixer(BaseEstimator, TransformerMixin):
+    debt_ratio_median_ : float
+
+    def __init__(self, debt_ratio_col = "DebtRatio",
+                 income_col = "MonthlyIncome",
+                 flag_missing_income_col = "missingindicator_MonthlyIncome"
+                 ):
+        self.debt_ratio_col = debt_ratio_col
+        self.income_col = income_col
+        self.flag_missing_income_col = flag_missing_income_col
+
+    def fit(self, X:DataFrame, y=None):
+        mask = (X[self.flag_missing_income_col] == 0)
+
+        if mask.any():
+            self.debt_ratio_median_ = X.loc[mask, self.debt_ratio_col].median()
+        else:
+            self.debt_ratio_median_ = 0.3
+        return self
+
+    def transform(self, X, y=None):
+        df = X.copy()
+
+        real_ratio_mask = (df[self.flag_missing_income_col] == 0)
+
+        df.loc[real_ratio_mask,"MonthlyDebtAmount"] = (
+                df.loc[real_ratio_mask,self.debt_ratio_col] *
+                df.loc[real_ratio_mask,self.income_col]
+        )
+        df.loc[~real_ratio_mask,"MonthlyDebtAmount"] = df.loc[~real_ratio_mask, self.debt_ratio_col]
+
+        df.loc[real_ratio_mask, "TrueDebtRatio"] = df.loc[real_ratio_mask, self.debt_ratio_col]
+        df.loc[~real_ratio_mask, "TrueDebtRatio"] = self.debt_ratio_median_
+
+        df.drop(labels=[self.debt_ratio_col], axis='columns', inplace=True)
+        return df
 
 def get_cleaner_pipeline() -> Pipeline:
     dependents_imputer = SimpleImputer(strategy='constant', fill_value=0)
@@ -78,13 +114,21 @@ def get_cleaner_pipeline() -> Pipeline:
     codes_list = [96,98]
     codes_flagger = CodesFlagger(columns_list=code_cols, codes_list=codes_list)
 
-    cols_to_cap = ["MonthlyIncome", "RevolvingUtilizationOfUnsecuredLines"]
+    debt_fixer = DebtRatioFixer()
+
+    cols_to_cap = [
+        "MonthlyIncome",
+        "RevolvingUtilizationOfUnsecuredLines",
+        "MonthlyDebtAmount",
+        "TrueDebtRatio"
+    ]
     quantile_capper = QuantileCapper(columns_list=cols_to_cap)
 
     # build final
     pipeline = Pipeline([
         ('codes flagger', codes_flagger),
         ('imputer', imputation_transformer),
+        ("debt_fixer", debt_fixer),
         ('capper', quantile_capper)
     ])
 
